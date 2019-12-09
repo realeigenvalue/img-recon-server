53f03e448be21311bab0a1ad336efa3145952140
realeigenvalue <57877219+realeigenvalue@users.noreply.github.com>
2019-12-08 22:45:35 -0600

    Add Main class

---
+++ src/main/java/com/realeigenvalue/img_recon_server/Main.java
@@ -0,0 +1,8 @@
+package com.realeigenvalue.img_recon_server;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ServerConsoleApp consoleApp = new ServerConsoleApp();
+	}
+}
